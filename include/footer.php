7da7ab55cf6b4bafd86640fa64d053e02f5f7d2e
Sudipto Mondal <sudipta.just@gmail.com>
2019-03-30 14:07:52 +0600

    Search Pagination updated

--- include/footer.php
+++ include/footer.php
@@ -1,8 +1,12 @@
     <!-- Jquery JS-->
     <script src="vendor/jquery-3.2.1.min.js"></script>
+    
     <!-- Bootstrap JS-->
     <script src="vendor/bootstrap-4.1/popper.min.js"></script>
     <script src="vendor/bootstrap-4.1/bootstrap.min.js"></script>
+    <script type="text/javascript" src="https://cdn.datatables.net/v/dt/dt-1.10.18/datatables.min.js"></script>
+    
+
     <!-- Vendor JS       -->
     <script src="vendor/slick/slick.min.js">
     </script>
@@ -11,8 +15,7 @@
     <script src="vendor/bootstrap-progressbar/bootstrap-progressbar.min.js">
     </script>
     <script src="vendor/counter-up/jquery.waypoints.min.js"></script>
-    <script src="vendor/counte
-    r-up/jquery.counterup.min.js">
+    <script src="vendor/counter-up/jquery.counterup.min.js">
     </script>
     <script src="vendor/perfect-scrollbar/perfect-scrollbar.js"></script>
     <script src="vendor/select2/select2.min.js">
--- purchase_report.php
+++ purchase_report.php
@@ -2,94 +2,76 @@
 require_once('include\db_connect.php');
 ?>
 <style>
-    .pagination-small li a{
-        padding: 5px;
-        border: 1px solid grey;
-        
+    th{
+        font-size: 15px;
     }
 </style>
 
 <?php $currentPage = 'product_report'; include 'include/header.php' ?>
             <!-- HEADER DESKTOP-->
-<script src="http://ajax.googleapis.com/ajax/libs/angularjs/1.5.7/angular.min.js"></script>
+
             <!-- MAIN CONTENT-->
             <div class="main-content">
                 <div class="section__content section__content--p30">
                     <div class="container-fluid">
                         <!-- Start to Copy From Here -->
                           
-                            <div class="row m-t-30" ng-app="myApp" ng-controller="controller">
-                            <div class="col-md-12">
-
-                                <!-- DATA TABLE-->
-                                <div class="table-responsive m-b-40" >
-                                    <div class="row">
-                                        <div class="col-sm-2 pull-left">
-                                            <label>PageSize:</label>
-                                            <select ng-model="data_limit" class="form-control">
-                                                <option>10</option>
-                                                <option>20</option>
-                                                <option>50</option>
-                                                <option>100</option>
-                                            </select>
-                                        </div>
-                                        <div class="col-sm-6 pull-right">
-                                            <label>Search:</label>
-                                            <input type="text" ng-model="search" ng-change="filter()" placeholder="Search" class="form-control" />
-                                        </div>
-                                    </div>
-                                    <table class="table table-borderless table-data3">
-
-                                        <br>
+                                <div class="container col-md-12">
+                                    <div class="row table-responsive m-b-40">
                                         
-                                        <thead>
-                                            <tr>
-                                                <th >SL NO</th>
-                                                <th>Company Name</th>
-                                                <th>Product Name</th>
-                                                <th>Purchase Unit Price</th>
-                                                <th>Purchase Ream</th>
-                                                <th>Total Amount</th>
-                                                <th>Purchase Date</th>
-                                                <th colspan="2" style="text-align: center;">Action</th>                               
-                                            </tr>
-                                        </thead>
-                                        
-                                        <tbody>
-                                            <tr ng-repeat="data in searched = (file | filter:search | orderBy : base :reverse) | beginning_data:(current_grid-1)*data_limit | limitTo:data_limit">
-                                                <td>{{$index+1}}</td>
-                                                <td>{{data.supplier_name}}</td>
-                                                <td>{{data.product_name}}</td>
-                                                <td>{{data.qty}}</td>
-                                                <td>{{data.buy_price}}</td>
-                                                <td>{{data.sale_price}}</td>
-                                                <td>{{data.order_date}}</td>
-                                                <td><a href="inc.process\edit_purchase_report_process.php?id=<?php echo $row['id']; ?>"><button type="button" class="btn btn-success">Edit</button></a></td>
-
-                                                <td><a href="inc.process/delete_product.php?id=<?php echo $row['id']; ?>"><button type="button" class="btn btn-danger" onclick=" return confirm('Sure you want to delete???');" >Delete</button></a></td>
-                                            </tr> 
-                                            
-                                        </tbody>
-                                    </table>
-
-                                </div>
-                                <!-- END DATA TABLE-->
-                               <div class="col-md-12" ng-show="filter_data == 0">
-                                   <div class="col-md-12">
-                                       <h4>No records found..</h4>
-                                   </div>
-                               </div>
-                               <div class="col-md-12">
-                                   <div class="col-md-6 pull-left">
-                                       <h5>Showing {{ searched.length }} of {{ entire_user}} entries</h5>
-                                   </div>
-                                   <div class="col-md-6" ng-show="filter_data > 0">
-                                       <div pagination="" page="current_grid" on-select-page="page_position(page)" boundary-links="true" total-items="filter_data" items-per-page="data_limit" class="pagination-small pull-right" previous-text="&laquo;" next-text="&raquo;"></div>
-                                   </div> 
                                     
+                                        <table id="example" class="display" >
+                                            <thead>
+                                                    <tr>
+                                                        <th >SL NO</th>
+                                                        <th>Supplier</th>
+                                                        <th>Product</th>
+                                                        <th>Quantity</th>
+                                                        <th>Buy Price</th>
+                                                        <th>sale Price</th>
+                                                        <th>Date</th>
+                                                        <th>edit</th>
+                                                        <th>delete</th>
+                                                                                       
+                                                    </tr>
+                                                </thead>
+                                                <?php
+                                                        $counter=0;
+                                                        
+                                                        $query="select * from purchase order by id DESC";
+                                                        $query_run=mysqli_query($connect, $query);
+                                                        while($row=mysqli_fetch_array($query_run)){
+                                                            $counter++;
+                                                        ?>
+                                                    <tr>
+
+                                                        <td ><?php echo $counter; ?></td>
+                                                        <td><?php echo $row['supplier_name']; ?></td>
+                                                        <td><?php echo $row['product_name']; ?></td>
+                                                        <td><?php echo $row['qty']; ?></td>
+                                                        <td><?php echo $row['buy_price']; ?></td>
+                                                        <td><?php echo $row['sale_price']; ?></td>
+                                                        <td ><?php echo date("d-m-Y", strtotime($row['order_date']));?></td>
+                                                        <td>
+                                                            <a href="inc.process\edit_purchase_report_process.php?id=<?php echo $row['id']; ?>"><button type="button" class="btn btn-success"><i class="fas fa-edit"></i></button></a>
+                                                           
+                                                        </td>
+                                                        <td>
+                                                             <a href="inc.process/delete_product.php?id=<?php echo $row['id']; ?>"><button type="button" class="btn btn-danger" onclick=" return confirm('Sure you want to delete???');" ><i class="fas fa-trash-alt"></i></button></a>
+                                                        </td>
+                                                        
+                                                        
+
+                                                    <?php  
+                                                    } ?>
+                                                    </tr>
+                                                <tbody>
+                                                   
+                                        
+                                                </tbody>
+                                            </table>
+                                    </div>
                                 </div>
-                            </div>
-                        </div>
                          
                         <!-- End the Copy From Here -->
                         <div class="row">
@@ -110,5 +92,10 @@ require_once('include\db_connect.php');
 
     <?php require 'include/footer.php' ?>
 <!-- end document-->
-    <script src="https://cdnjs.cloudflare.com/ajax/libs/angular-ui-bootstrap/0.10.0/ui-bootstrap-tpls.min.js"></script>
-    <script src="myapp.js"></script>
+    
+    
+    <script>
+        $(document).ready(function() {
+        $('#example').DataTable();
+    } );
+    </script>
